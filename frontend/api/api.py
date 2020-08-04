9cf59734b202886564105fca2c8ae0fa838a1612
Siddhesh Krishnan <siddheshkrishnan1@gmail.com>
2020-08-03 22:00:11 -0500

    Added API boilerplate template, might need fixing

--- frontend/api/api.py
+++ frontend/api/api.py
@@ -5,6 +5,26 @@ app = Flask(__name__)
 def homepage():
     return "This is the homepage"
 
-@app.route('/test')
+@app.route('/test', methods=['POST'])
 def get_page_name():
-    return {'name': "Storage Calculator"};
+    lr = joblib.load("model.pkl") 
+    print ('Model loaded')
+    model_columns = joblib.load("model_columns.pkl") 
+    print ('Model columns loaded')
+    if lr:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        return ('No model here to use')
+    #return {'name': "Storage Calculator"};
